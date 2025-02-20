355b990006523fa96ca67746dcc0409e213276a6
= <=>
2025-02-21 03:49:54 +0600

    Authorized user verification added

--- routes/router.js
+++ routes/router.js
@@ -1,6 +1,7 @@
 import express from "express";
 import { createAccesToken } from "../controllers/Auth/createToken.js";
 import { createNewTask } from "../controllers/Task/createNewTask.js";
+import { verifyAccessToken } from "../middlewares/verifyAccessToke.js";
 
 const router = express.Router();
 
@@ -8,7 +9,7 @@ const router = express.Router();
 router.post(`/auth/token/new`, createAccesToken);
 
 //  Task Managment
-router.post(`/task/new`, createNewTask);
+router.post(`/task/new`, verifyAccessToken, createNewTask);
 
 // Default
 router.get("*", (req, res) => {
